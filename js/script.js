459d6861860a9ce8c968666bf7903e7c83015c55
Ângelo <98216100+angelodesenvolvedor@users.noreply.github.com>
2025-06-17 22:16:47 -0300

    fix: atualizar URL base da API para o ambiente de produção

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const apiService = {
-    BASE_URL: "http://localhost:3000/api",
+    BASE_URL: "https://nexttaskweb.vercel.app/",
 
     async _fetch(endpoint, options = {}) {
       const currentUserData = JSON.parse(localStorage.getItem("currentUser"));

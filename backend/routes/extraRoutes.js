27aed43acb382948ad6267c828bbe8777d2ed085
Ângelo <98216100+angelodesenvolvedor@users.noreply.github.com>
2025-06-17 21:08:22 -0300

    feat: adicionar rotas para gerenciamento de comentários e histórico de tarefas

---
+++ backend/routes/extraRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+const activityLogController = require('../controllers/activityLogController');
+const authMiddleware = require('../middleware/authMiddleware');
+
+// Protege todas estas rotas
+router.use(authMiddleware);
+
+// Rotas de Comentários
+router.get('/tasks/:taskId/comments', commentController.getCommentsForTask);
+router.post('/tasks/:taskId/comments', commentController.addCommentToTask);
+
+// Rota de Histórico
+router.get('/tasks/:taskId/history', activityLogController.getHistoryForTask);
+
+module.exports = router;

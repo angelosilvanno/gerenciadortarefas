1f29548265a24fe495e45faf134d658fb0e98a0d
Ângelo <98216100+angelodesenvolvedor@users.noreply.github.com>
2025-06-17 22:07:47 -0300

    fix: corrigir espaço extra na URL de origem do CORS

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 const corsOptions = {
   origin: [
-    'https://nexttaskapp.netlify.app', 
+    'https:// nexttaskapp.netlify.app', 
     'http://127.0.0.1:5500'
   ],
   optionsSuccessStatus: 200

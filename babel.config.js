ff9f85c411b5735f4752deb29c8db95739b647a0
Ludmila Monteiro <milacostamonteiro@hotmail.com>
2025-06-19 20:41:34 -0300

    fix(ci): ajusta babel.config.js para CommonJS no GitHub Actions

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
-export default {
+module.exports = {
     presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
     plugins: ['@babel/plugin-transform-modules-commonjs']
   };
+  
   

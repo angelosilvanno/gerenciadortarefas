c1588a0f111b0ad9388e7fda6bf6e94b5e35e52e
Ludmila Monteiro <milacostamonteiro@hotmail.com>
2025-06-20 22:16:32 -0300

    fix(ci): recria jest.config.js sem jest-html-reporter

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    testEnvironment: 'node',
+    collectCoverage: true,
+    collectCoverageFrom: [
+      'backend/utils/**/*.js',
+      'backend/middleware/**/*.js',
+      'backend/models/**/*.js',
+    ],
+    coverageDirectory: 'coverage',
+    reporters: ['default']
+  };
+  

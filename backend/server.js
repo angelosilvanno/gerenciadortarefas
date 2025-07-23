8dbc080b2ca84823aa6b5969e6c2052a0391a53f
Levitico Rimon <leviticorimon@gmail.com>
2025-07-23 15:12:58 -0300

    feat: adiciona configuração de cors

--- backend/server.js
+++ backend/server.js
@@ -6,3 +6,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'https://nextask-app.onrender.com'
+};
+app.use(cors(corsOptions));

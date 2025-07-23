822aada456f3c87b59ba35ec9e32b2ae7a9b83a7
Levitico Rimon <leviticorimon@gmail.com>
2025-07-23 15:40:34 -0300

    fix: corrige configuração de cors

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
 const app = require('./app');
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
 const cors = require('cors');
 
 const corsOptions = {
   origin: 'https://nextask-app.onrender.com'
 };
 app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
